ee2f441f2ff25f125cfe0bd063fdc7951f4fe72d
Francisco Cobas <francisco.cobas@modelitsoftware.com>
2015-02-18 20:49:14 -0200

    Add more functionality to the cover photo

--- app/assets/javascripts/angular-app/controllers/profileController.js
+++ app/assets/javascripts/angular-app/controllers/profileController.js
@@ -8,7 +8,6 @@ angular.module('mepedia.controllers').controller('profileController',
 //            $scope.logout = function() {
 //                sessionService.logout()
 //            }
-
             $scope.onFileSelect = function($files) {
 
                 $.cloudinary.config({
@@ -17,6 +16,9 @@ angular.module('mepedia.controllers').controller('profileController',
                 });
 
                 var file = $files[0]; // we're not interested in multiple file uploads here
+
+                var picture;
+
                 $scope.upload = $upload.upload({
                     url: "https://api.cloudinary.com/v1_1/" + $.cloudinary.config().cloud_name + "/upload",
                     data: {upload_preset: $.cloudinary.config().upload_preset, tags: 'myphotoalbum', context:'photo=' + $scope.title},
@@ -24,15 +26,44 @@ angular.module('mepedia.controllers').controller('profileController',
                 }).progress(function (e) {
                     $scope.progress = Math.round((e.loaded * 100.0) / e.total);
                     $scope.status = "Uploading... " + $scope.progress + "%";
-                    $scope.$apply();
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
                 }).success(function (data, status, headers, config) {
                     $rootScope.photos = $rootScope.photos || [];
                     data.context = {custom: {photo: $scope.title}};
                     $scope.result = data;
                     $rootScope.photos.push(data);
-                    $scope.$apply();
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    } else {
+                        $scope.data = data;
+                    }
+                }).then(function (success, error, progress) {
+
                 });
+                
             };
+            var picture = angular.element('#sample_picture');
+            var content = angular.element('.content');
+            picture.on('load', function() { 
+                picture.guillotine({eventOnChange: 'guillotinechange', width: content[0].offsetWidth, height: 315});
+
+                var data = picture.guillotine('getData');
+                console.log(data);
+                for(var key in data) { $('#'+key).html(data[key]); }
+
+                $('#rotate_left').click(function(){ picture.guillotine('rotateLeft'); });
+                $('#rotate_right').click(function(){ picture.guillotine('rotateRight'); });
+                $('#fit').click(function(){ picture.guillotine('fit'); });
+                $('#zoom_in').click(function(){ picture.guillotine('zoomIn'); });
+                $('#zoom_out').click(function(){ picture.guillotine('zoomOut'); });
+
+                picture.on('guillotinechange', function(ev, data, action) {
+                  data.scale = parseFloat(data.scale.toFixed(4));
+                  for(var k in data) { $('#'+k).html(data[k]); }
+                });
+            });
 
             / Modify the look and fill of the dropzone when files are being dragged over it /
             $scope.dragOverClass = function($event) {
@@ -50,6 +81,4 @@ angular.module('mepedia.controllers').controller('profileController',
                 }
                 return hasFile ? "dragover" : "dragover-err";
             };
-}]);
-
-
+        }]);
