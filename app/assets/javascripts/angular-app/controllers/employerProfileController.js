798daa9e55a76e05849c9b0c24f3060a02400315
Francisco Cobas <francisco.cobas@modelitsoftware.com>
2015-03-28 15:45:16 -0300

    Add employer login and basic components

---
+++ app/assets/javascripts/angular-app/controllers/employerProfileController.js
@@ -0,0 +1,10 @@
+angular.module('mepedia.controllers').controller('candidateProfileController',
+    ['$scope', '$rootScope', '$http', '$upload', 'sessionService',
+
+        function($scope, $rootScope,$httpProvider, $upload, sessionService, ) {
+
+
+
+        }]);
+
+
