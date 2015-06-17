e49ba5a75891426ac8730ac83363d30651f7bf45
Mariana <mariana.ardoino@gmail.com>
2015-06-17 00:53:18 -0300

    tags input input and add button in the same line

--- app/assets/javascripts/angular-app/directives/tagsInput.js
+++ app/assets/javascripts/angular-app/directives/tagsInput.js
@@ -5,11 +5,12 @@ angular.module('mepedia.directives').directive('tagsInput', function () {
         template: function (elem, attr) {
             var placeholder = attr.placeholder ? attr.placeholder : '';
             var signupClass = attr.signup == "" ? 'tags-box' : '';
-            return '<div class="tagsinput cont left">' +
-            '<input type="text" ng-model="new_value" class="input-sm form-control tagsInput" ' +
-            'typeahead="tag as tag.name for tag in tags | filter:$viewValue | limitTo:6" placeholder="' + placeholder + '">' +
-            '</div>' + '<div class="tagsinput cont right">' +
+            return '<div class="tagsinput-right">' +
             '<a class="tagsinput addbutton btn-sm" ng-click="add()">Add</a>' + '</div>' +
+            '<div class="tagsinput-left">' +
+            '<input type="text" ng-model="new_value" class="input-sm form-control"' +
+            'typeahead="tag as tag.name for tag in tags | filter:$viewValue | limitTo:6" placeholder="' + placeholder + '">' +
+            '</div>' +
             '<div class="tags ' + signupClass + '">' +
             '<a class="tag" ng-model="flavor" ng-repeat="(idx, tag) in currentTags" ng-click="remove(idx)">{{tag}}</a>' +
             '</div>'
