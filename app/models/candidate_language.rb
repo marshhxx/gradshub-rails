671bf0e102ebb5f073917b54c3a329c1e4c92150
Mariana <mariana.ardoino@gmail.com>
2015-06-20 19:28:51 -0300

    languages + borders + colors + btns profile

--- app/models/candidate_language.rb
+++ app/models/candidate_language.rb
@@ -1,5 +1,5 @@
 class CandidateLanguage < ActiveRecord::Base
   belongs_to :candidate
   belongs_to :language
-  enum level: {begginer: 0, intermidiate: 1, advanced:2}
+  enum level: {begginer: 0, intermediate: 1, advanced:2}
 end
