8ddba94665fcb9cb767e354a6e5124083201f75a
Francisco Cobas <francisco.cobas@modelitsoftware.com>
2015-08-15 12:54:11 -0300

    Added missed file

--- app/assets/javascripts/angular-app/controllers/loginController.js
+++ app/assets/javascripts/angular-app/controllers/loginController.js
@@ -1,6 +1,6 @@
 angular.module('mepedia.controllers').controller('loginController',  
-    ['$scope', '$state', 'sessionService', 'alertService', '$sce',
-        function ($scope, $state, sessionService, alertService, $sce) {
+    ['$scope', '$state', 'sessionService', 'alertService', '$sce', '$analytics',
+		function ($scope, $state, sessionService, alertService, $sce, $analytics) {
             var randomLogin = function () {
                 var images = ['one', 'other'];
                 return images[Math.floor((Math.random() * 2) + 1) - 1];
@@ -18,14 +18,21 @@ angular.module('mepedia.controllers').controller('loginController',
                         var promise = sessionService.login($scope.email, $scope.password, false);
                         promise.then(
                             function (resp) {
-                                if (resp.type == 'Candidate')
+                                if (resp.type == 'Candidate') {
+                                    // Log event in Google Analytics
+                                    $analytics.eventTrack('Login Candidate', { category: 'Candidate', label: 'Internal Login Candidate' });
+
                                     $state.go('main.candidate_profile', {uid: 'me'});
-                                else if (resp.type == 'Employer')
+                                } else if (resp.type == 'Employer') {
+                                    // Log event in Google Analytics
+                                    $analytics.eventTrack('Login Employer', { category: 'Employer', label: 'Internal Login Employer' });
+                                    
                                     $state.go('main.employer_profile', {uid: 'me'});
+                                }   
                             }
                         ).catch(
                             function (resp) {
-                                alertService.addError(resp.error, 10000)
+                                alertService.addError(resp.error, 10000);
                             }
                         );
                     }
