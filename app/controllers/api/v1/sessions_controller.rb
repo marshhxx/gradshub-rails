f5c86528cef47eea19f69b90e81af60cd5f48aea
Martin Bomio <martinbomio@gmail.com>
2015-10-03 16:02:29 -0300

    remove secret.yml

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::SessionsController < ApplicationController
+  before_action :authenticate_with_token!, only: [:refresh]
 
   def create
     user_password = params[:session][:password]
