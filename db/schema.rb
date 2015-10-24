4a55a563bf7958132be74397b7ee5f1450b92fed
Francisco Cobas <fcarocena@gmail.com>
2015-10-24 17:28:52 -0300

    Remove all references to Employer company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024142658) do
+ActiveRecord::Schema.define(version: 20151024183633) do
 
   create_table "candidate_interests", force: true do |t|
     t.integer "candidate_id"
