e8badba352979f55f1952b72bcab9906a1fa8529
Francisco Cobas <fcarocena@gmail.com>
2015-10-26 18:00:24 -0300

    latest changes from employer re design

--- app/controllers/api/v1/employers_controller.rb
+++ app/controllers/api/v1/employers_controller.rb
@@ -1,30 +1,26 @@
 class Api::V1::EmployersController < Api::V1::UsersController
   wrap_parameters include: [:name, :lastname, :email, :password, :gender,
                               :birth, :country_id, :state_id, :profile_image, 
-                              :cover_image, :tag, :job_title, :company_image,
-                              :new_password, :old_password]
+                              :cover_image, :tag, :job_title, :company_name,
+                              :company_logo, :company_tagline, :company_url,
+                              :description, :new_password, :old_password]
 
   private
 
   def employer_params
     nest_user_attributes params.require(:employer).permit(
        :name, :lastname, :email, :password, :gender,
-       :birth, :country_id, :state_id, :profile_image, :cover_image, :tag, :job_title, :company_image
+       :birth, :country_id, :state_id, :profile_image, :cover_image, :tag, :job_title, :company_name,
+       :company_logo, :company_tagline, :company_url, :description
     ) if params[:employer]
   end
 
   def update_params
     nest_user_attributes params.require(:employer).permit(
        :name, :lastname, :gender,
-       :birth, :country_id, :state_id, :profile_image, :cover_image, :tag, :job_title, :company_image
+       :birth, :country_id, :state_id, :profile_image, :cover_image, :tag, :job_title, :company_name,
+       :company_logo, :company_tagline, :company_url, :description
     ) if params[:employer]
   end
 
-  def company_params
-    params.require(:employer).permit(:company => [:name, :industry, :description])
-  end
-
-  def location_params
-    params.require(:employer).permit(:company => [:locations => [:country_id, :state_id]])
-  end
 end
