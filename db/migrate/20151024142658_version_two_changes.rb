af96128b09a279a3fa324deeeab2662b114968a1
Francisco Cobas <fcarocena@gmail.com>
2015-11-08 09:33:40 -0300

    Modify front-end to integrate employer changes

--- db/migrate/20151024142658_version_two_changes.rb
+++
@@ -1,8 +0,0 @@
-class VersionTwoChanges < ActiveRecord::Migration
-
-  def change
-    change_column_null(:educations, :start_date, true)
-    change_column_null(:educations, :end_date, false, DateTime.now)
-  end
-
-end
--- db/migrate/20151024183633_modify_company.rb
+++ db/migrate/20151024183633_modify_company.rb
@@ -1,6 +1,9 @@
 class ModifyCompany < ActiveRecord::Migration
   def change
 
+    change_column_null(:educations, :start_date, true)
+    change_column_null(:educations, :end_date, false, DateTime.now)
+
     change_table :employers do |t|
       t.string :company_name, :company_logo, :company_tagline, :company_url, :company_industry, :company_description
     end
