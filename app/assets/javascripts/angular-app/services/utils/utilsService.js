020e214f16612b4b7a16a8dc0261a51c80562740
Francisco Cobas <fcarocena@gmail.com>
2015-11-14 13:04:02 -0300

    improve code

--- app/assets/javascripts/angular-app/services/utils/utilsService.js
+++ app/assets/javascripts/angular-app/services/utils/utilsService.js
@@ -43,22 +43,21 @@ var Utils = function(Candidate, Employer, $stateParams) {
         employer.gender = user.gender;
         employer.birth = user.birth;
         employer.tag = user.tag;
-        employer.company_image = user.company_image;
         employer.profile_image = user.profile_image;
         employer.cover_image = user.cover_image;
         employer.job_title = user.job_title;
-        if (user.country && user.state) {
-            employer.country_id = user.country.id;
-            employer.state_id = user.state.id;
-        }
         employer.company = user.company;
-
         employer.company_name = user.company_name;
         employer.company_logo = user.company_logo;
         employer.company_tagline = user.company_tagline;
         employer.company_url = user.company_url;
         employer.company_description = user.company_description;
         employer.company_industry = user.company_industry;
+
+        if (user.country && user.state) {
+            employer.country_id = user.country.id;
+            employer.state_id = user.state.id;
+        }
 
         return employer;
     };
